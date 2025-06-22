cf7cc7b0fc6d533e63c054d12b636240b1147d86
nsgtesting <nsgterraform@gmail.com>
2025-06-22 21:53:24 +0530

    skipping4

--- main.tf
+++ main.tf
@@ -30,6 +30,24 @@ resource "azurerm_network_security_group" "testnsg" {
                 source_port_range                          = "*"
                 source_port_ranges                         = []
             },
+            {
+                access                                     = "Allow"
+                description                                = null
+                destination_address_prefix                 = "*"
+                destination_address_prefixes               = []
+                destination_application_security_group_ids = []
+                destination_port_range                     = "80"
+                destination_port_ranges                    = []
+                direction                                  = "Inbound"
+                name                                       = "allow80"
+                priority                                   = 300
+                protocol                                   = "*"
+                source_address_prefix                      = "*"
+                source_address_prefixes                    = []
+                source_application_security_group_ids      = []
+                source_port_range                          = "*"
+                source_port_ranges                         = []
+            },
         ]
 }
  
