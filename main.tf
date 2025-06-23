9aeb214d02933a3a0a5fcfba3823b49eaa6ef362
nsgtesting <nsgterraform@gmail.com>
2025-06-23 10:58:28 +0530

    skippinall123

--- main.tf
+++ main.tf
@@ -37,19 +37,19 @@ resource "azurerm_network_security_group" "nsgimp" {
   dynamic "security_rule" {
     for_each = var.security_rules
     content {
-      name                       = each.value.name
-      priority                   = each.value.priority
-      direction                  = each.value.direction
-      access                     = each.value.access
-      protocol                   = each.value.protocol
-      source_port_range          = try(each.value.source_port_range, null)
-      source_port_ranges         = try(each.value.source_port_ranges, null)
-      destination_port_range     = try(each.value.destination_port_range, null)
-      destination_port_ranges    = try(each.value.destination_port_ranges, null)
-      source_address_prefix      = try(each.value.source_address_prefix, null)
-      source_address_prefixes    = try(each.value.source_address_prefixes, null)
-      destination_address_prefix = try(each.value.destination_address_prefix, null)
-      destination_address_prefixes = try(each.value.destination_address_prefixes, null)
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = try(security_rule.value.source_port_range, null)
+      source_port_ranges         = try(security_rule.value.source_port_ranges, null)
+      destination_port_range     = try(security_rule.value.destination_port_range, null)
+      destination_port_ranges    = try(security_rule.value.destination_port_ranges, null)
+      source_address_prefix      = try(security_rule.value.source_address_prefix, null)
+      source_address_prefixes    = try(security_rule.value.source_address_prefixes, null)
+      destination_address_prefix = try(security_rule.value.destination_address_prefix, null)
+      destination_address_prefixes = try(security_rule.value.destination_address_prefixes, null)
     }
   }
 }
