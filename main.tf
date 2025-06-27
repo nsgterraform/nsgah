c8abd04cedd80a7a6d57a0238f7e0fe444847587
nsgterraform <nsgterraform@gmail.com>
2025-06-27 15:27:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,41 @@ resource "azurerm_network_security_group" "oribnsg" {
   resource_group_name = data.azurerm_resource_group.rgname.name
 
   dynamic "security_rule" {
+    for_each = { for rule in var.orib_securityrule : rule.name => rule }
+
+    content {
+      name                         = security_rule.value.name
+      priority                     = security_rule.value.priority
+      direction                    = security_rule.value.direction
+      access                       = security_rule.value.access
+      protocol                     = security_rule.value.protocol
+
+      source_port_range            = try(security_rule.value.source_port_range, "")
+      source_port_ranges           = try(security_rule.value.source_port_ranges, [])
+
+      destination_port_range       = try(security_rule.value.destination_port_range, "")
+      destination_port_ranges      = try(security_rule.value.destination_port_ranges, [])
+
+      source_address_prefix        = try(security_rule.value.source_address_prefix, "")
+      source_address_prefixes      = try(security_rule.value.source_address_prefixes, [])
+
+      destination_address_prefix   = try(security_rule.value.destination_address_prefix, "")
+      destination_address_prefixes = try(security_rule.value.destination_address_prefixes, [])
+
+      destination_application_security_group_ids = try(security_rule.value.destination_application_security_group_ids, [])
+      source_application_security_group_ids      = try(security_rule.value.source_application_security_group_ids, [])
+    }
+  }
+
+  tags = {}
+}
+
+/*resource "azurerm_network_security_group" "oribnsg" {
+  name                = "oribnsg"
+  location            = "East US"
+  resource_group_name = data.azurerm_resource_group.rgname.name
+
+  dynamic "security_rule" {
     for_each = { for rule in sort(var.orib_securityrule[*].name) : rule => lookup({ for r in var.orib_securityrule : r.name => r }, rule) }
 
     content {
@@ -37,4 +72,4 @@ resource "azurerm_network_security_group" "oribnsg" {
   }
 
   tags = {}
-}
+}*/
