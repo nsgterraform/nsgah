3afd6e38df9cd3741537a9bd0c96d357e6475444
nsgterraform <nsgterraform@gmail.com>
2025-06-27 15:02:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,20 +19,20 @@ resource "azurerm_network_security_group" "oribnsg" {
       access                       = security_rule.value.access
       protocol                     = security_rule.value.protocol
 
-      source_port_range            = security_rule.value.source_port_range != null && length(trimspace(security_rule.value.source_port_range)) > 0 ? security_rule.value.source_port_range : null
-      source_port_ranges           = length(security_rule.value.source_port_ranges) > 0 ? security_rule.value.source_port_ranges : null
+      source_port_range            = try(security_rule.value.source_port_range, null)
+      source_port_ranges           = try(security_rule.value.source_port_ranges, [])
 
-      destination_port_range       = security_rule.value.destination_port_range != null && length(trimspace(security_rule.value.destination_port_range)) > 0 ? security_rule.value.destination_port_range : null
-      destination_port_ranges      = length(security_rule.value.destination_port_ranges) > 0 ? security_rule.value.destination_port_ranges : null
+      destination_port_range       = try(security_rule.value.destination_port_range, null)
+      destination_port_ranges      = try(security_rule.value.destination_port_ranges, [])
 
-      source_address_prefix        = security_rule.value.source_address_prefix != null && length(trimspace(security_rule.value.source_address_prefix)) > 0 ? security_rule.value.source_address_prefix : null
-      source_address_prefixes      = length(security_rule.value.source_address_prefixes) > 0 ? security_rule.value.source_address_prefixes : null
+      source_address_prefix        = try(security_rule.value.source_address_prefix, null)
+      source_address_prefixes      = try(security_rule.value.source_address_prefixes, [])
 
-      destination_address_prefix   = security_rule.value.destination_address_prefix != null && length(trimspace(security_rule.value.destination_address_prefix)) > 0 ? security_rule.value.destination_address_prefix : null
-      destination_address_prefixes = length(security_rule.value.destination_address_prefixes) > 0 ? security_rule.value.destination_address_prefixes : null
+      destination_address_prefix   = try(security_rule.value.destination_address_prefix, null)
+      destination_address_prefixes = try(security_rule.value.destination_address_prefixes, [])
 
-      destination_application_security_group_ids = length(security_rule.value.destination_application_security_group_ids) > 0 ? security_rule.value.destination_application_security_group_ids : null
-      source_application_security_group_ids      = length(security_rule.value.source_application_security_group_ids) > 0 ? security_rule.value.source_application_security_group_ids : null
+      destination_application_security_group_ids = try(security_rule.value.destination_application_security_group_ids, [])
+      source_application_security_group_ids      = try(security_rule.value.source_application_security_group_ids, [])
     }
   }
 
