cc222850e92899c66056bf67bfb41f11d48c6671
nsgterraform <nsgterraform@gmail.com>
2025-07-01 13:40:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,14 @@ resource "azurerm_network_security_rule" "somensgs" {
   direction                   = each.value.direction
   access                      = each.value.access
   protocol                    = each.value.protocol
-  source_port_range           = each.value.source_port_range
-  destination_port_range      = each.value.destination_port_range
-  source_address_prefix       = each.value.source_address_prefix
-  destination_address_prefix  = each.value.destination_address_prefix
+  source_port_range           = try(each.value.source_port_range,null)
+  destination_port_range      = try(each.value.destination_port_range,null)
+  source_address_prefix       = try(each.value.source_address_prefix,null)
+  destination_address_prefix  = try(each.value.destination_address_prefix,null)
+  source_port_ranges           = try(each.value.source_port_ranges,null)
+  destination_port_ranges      = try(each.value.destination_port_ranges,null)
+  source_address_prefixes       = try(each.value.source_address_prefixes,null)
+  destination_address_prefixes  = try(each.value.destination_address_prefixes,null)
   resource_group_name         = data.azurerm_resource_group.rgname.name
   network_security_group_name = azurerm_network_security_group.indnsg.name
 }
