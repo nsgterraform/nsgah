f3ef6a663ab0bd5a3d6078492be5b08f0d4ee4cf
nsgtesting <nsgterraform@gmail.com>
2025-07-09 09:39:28 +0530

    added some important12

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "nsgtestmodule" {
   source = "./Modules/NSG"
   nsgname = var.nsgname
   location = var.location
-  rgnsgname = data.azurerm_resource_group.rgname
+  rgnsgname = data.azurerm_resource_group.rgname.name
   subnetids = data.azurerm_subnet.subnetfetch
   inboundrules = var.inboundrules
 }
