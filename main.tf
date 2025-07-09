3b2af5f24f6beedd3776fdafed869b8f40f13791
nsgterraform <nsgterraform@gmail.com>
2025-07-09 10:40:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ module "nsgtestmodule" {
   nsgname = var.nsgname
   location = var.location
   rgnsgname = data.azurerm_resource_group.rgname.name
-  subnetids = [data.azurerm_subnet.subnetfetch.id]
+  subnetids = ["/subscriptions/192c8ca3-9a0e-45a3-acf7-e131e5856927/resourceGroups/nsgtest/providers/Microsoft.Network/virtualNetworks/vnet-nsg/subnets/db-subnet"]
   inboundrules = var.inboundrules
 }
